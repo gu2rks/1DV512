97a23f36591ff8f50df2f62b4d79bf2b4898e41f
CHEAB <aa224iu@student.lnu.se>
2018-11-10 16:40:43 +0100

    Junit runs well

--- Task1/src/FCFS.java
+++ Task1/src/FCFS.java
@@ -1,35 +1,81 @@
+
 /*
  * File:	Process.java
  * Course: 	Operating Systems
  * Code: 	1DV512
  * Author: 	Suejb Memeti
+ * Refactoring by: Amata Anantaprayoon (aa224iu)
  * Date: 	November, 2018
  */
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
-public class FCFS{
+public class FCFS {
 
 	// The list of processes to be scheduled
 	public ArrayList<Process> processes;
+	private int m_completedTime;
 
 	// Class constructor
 	public FCFS(ArrayList<Process> processes) {
-		this.processes = processes;
+		this.processes = sortByAT(processes);
+		this.m_completedTime = 0;
 	}
 
 	public void run() {
-		// TODO Implement the FCFS algorithm here
+		for (Process a_process : processes) {
+			a_process.setCompletedTime(calculateCT(a_process));
+			a_process.setTurnaroundTime(calculateTAT(a_process));
+			a_process.setWaitingTime(calculateWT(a_process));
+		}
 
 	}
 
 	public void printTable() {
-		// TODO Print the list of processes in form of a table here
+		System.out.println("| Process Id | Arrival Time | Burst Time | Completed Time | Turnaround Time | Waiting Time |");
+		System.out.println("|____________|______________|____________|________________|_________________|______________|");
 
+		for (Process a_process : processes) {
+			System.out.printf("|%7s     |%9s     |%7s     |%11s     |%12s     |%9s     |\n", a_process.getProcessId(),
+					a_process.getArrivalTime(), a_process.getBurstTime(), a_process.getCompletedTime(),
+					a_process.getTurnaroundTime(), a_process.getWaitingTime());
+			System.out.println("|____________|______________|____________|________________|_________________|______________|");
+		}
 	}
 
 	public void printGanttChart() {
 		// TODO Print the demonstration of the scheduling algorithm using Gantt Chart
 
+	}
+
+	private ArrayList<Process> sortByAT(ArrayList<Process> m_processes) {
+		Collections.sort(m_processes, new Comparator<Process>() {
+			@Override
+			public int compare(Process p1, Process p2) {
+				return p1.getArrivalTime() - p2.getArrivalTime();
+			}
+		});
+		return m_processes;
+	}
+
+	private int calculateCT(Process a_process) {
+		if (a_process.getArrivalTime() <= m_completedTime) {
+			m_completedTime = m_completedTime + a_process.getBurstTime();
+		} else {
+			m_completedTime = a_process.getArrivalTime() + a_process.getBurstTime();
+		}
+		return m_completedTime;
+	}
+
+	private int calculateTAT(Process a_process) {
+		int tat = a_process.getCompletedTime() - a_process.getArrivalTime();
+		return tat;
+	}
+
+	private int calculateWT(Process a_process) {
+		int wt = a_process.getTurnaroundTime() - a_process.getBurstTime();
+		return wt;
 	}
 }
